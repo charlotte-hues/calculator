26b0d61dceb2ffaa16861fc2a1b99ee4f37cb34b
Charlotte Hughes <hues.charlotte@gmail.com>
2019-12-30 18:09:07 +0000

    All working as it should before i try making the operations work

--- scripts/calcFuncts.js
+++ scripts/calcFuncts.js
@@ -1,27 +1,6 @@
-function add (a, b) {
-	return a + b;
-};
+function add(a, b) { return a + b;};
+function subtract(a, b) { return a - b;};	
+function multiply(a, b) { return a * b;};
+function divide(a, b) {	return a / b;};
 
-function subtract (a, b) {
-	 return a - b;
-};	
-
-function sum (array) {
-	return array.reduce((last, current) => last + current, 0);
-};
-
-function multiply (array) { 
-	return array.reduce((last, current) =>  last * current)
-};
-
-function power(a, power) {
-	return Math.pow(a, power);
-};
-
-function factorial(a) {
-	let product = 1;
-	for (let i=a; i>0; i--) {
-		product *= i;
-	}
-	return product;
-};
+function operate(a, b, operation) { return operation(a,b); };
--- scripts/createCalc.js
+++ scripts/createCalc.js
@@ -1,4 +1,11 @@
-// BUTTONS - done like this to give each button a an id that isn't a number or a symbol.
+function add(a, b) { return a + b;};
+function subtract(a, b) { return a - b;};	
+function multiply(a, b) { return a * b;};
+function divide(a, b) {	return a / b;};
+
+function operate(a, b, operation) { return operation(a,b); };
+
+// BUTTONS - Done like this to give each button an id that isn't a number or a symbol so it can be styled with css if wanted.
 
 const clearButton = {id: 'clear', text: 'c'};
 const clearAllButton = {id: 'clearAll', text: 'ce'};
@@ -9,9 +16,9 @@ const subtractButton = {id: 'subtract', text: '-'};
 const multiplyButton = {id: 'multiply', text: 'x'};
 const divideButton = {id: 'divide', text: '/'};
 const backButton = {id: 'backspace', text: '<'};
-const swapButton = {id: 'swap', text: '+-'};
+const swapButton = {id: 'swap', text: '+/-'};
 
-const decimalButton = {id: 'decimal', text: 'x'};
+const decimalButton = {id: 'decimal', text: '.'};
 const zeroButton = {id: 'zero', text: '0'};
 const oneButton = {id: 'one', text: '1'};
 const twoButton = {id: 'two', text: '2'};
@@ -23,15 +30,15 @@ const sevenButton = {id: 'seven', text: '7'};
 const eightButton = {id: 'eight', text: '8'};
 const nineButton = {id: 'nine', text: '9'};
 
-const buttonsContainer = document.querySelector("#buttonsContainer");
-
-const functions = [ swapButton,     clearAllButton, clearButton,    backButton,
+const buttons   = [ swapButton,     clearAllButton, clearButton,    backButton,
                     sevenButton,    eightButton,    nineButton,     divideButton,
                     fourButton,     fiveButton,     sixButton,      multiplyButton,
                     oneButton,      twoButton,      threeButton,    subtractButton,
                     zeroButton,     decimalButton,  equalsButton,   addButton  ];
 
-function createFunctions(item) {
+const buttonsContainer = document.querySelector("#buttonsContainer");
+
+function createButtons(item) {
     let functionButton = document.createElement('button');
     buttonsContainer.appendChild(functionButton);
     functionButton.id = item.id;
@@ -39,8 +46,46 @@ function createFunctions(item) {
 
     if (functionButton.innerHTML >= 0 && functionButton.innerHTML <= 9 || functionButton.id === 'decimal' ) {
         functionButton.classList.add('numbers');
-    } else { functionButton.classList.add('functions');
+    } else { functionButton.classList.add('operators');
     }
 }
 
-functions.forEach(createFunctions);
+buttons.forEach(createButtons);
+
+const numbers = document.querySelectorAll('.numbers');
+const operators = document.querySelectorAll('.operators');
+const display = document.querySelector('#display');
+const resultDisplay = document.querySelector('#result');
+let displayValueA = '';
+let displayValueB = '';
+let operator;
+let count = 0;
+
+numbers.forEach(button => {
+    button.addEventListener('click', event => {
+        if(count===0) {
+            displayValueA = displayValueA + event.target.innerHTML;
+            display.innerHTML = displayValueA;  
+        } else {
+            displayValueB = displayValueB + event.target.innerHTML;
+            display.innerHTML = displayValueA + ' ' + displayValueB;              
+        }
+
+    })
+});
+
+
+operators.forEach(button => {
+    button.addEventListener('click', event => {
+        console.log(event.target.id);
+        display.innerHTML = displayValueA + event.target.innerHTML;
+        operator = event.target.id;
+        count++;
+        console.log(count);
+        console.log(operator);
+    })
+});
+
+
+document.querySelector('#equals').addEventListener('click', event => { resultDisplay.innerHTML = operate(displayValueA, displayValueB, operator); })
+
